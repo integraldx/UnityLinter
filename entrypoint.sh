e1809cdb7dc08a4cc9856f82510a077554780870
integraldx <integraldx@integraldx.me>
2020-09-01 07:00:37 +0900

    Wait for file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,17 @@
 #!/bin/bash -l
 
-/opt/Unity/Editor/Unity -batchmode -nographics -projectPath /project -runTests -testResults /project/testResults.xml -logFile /project/Editor.log -quit
+wait_file() {
+    local file="$1"; shift
+    local wait_seconds="${1:-10}"; shift
+
+    until test $((wait_seconds--)) -eq 0 -o -e "$file" ; do sleep 1; done
+
+    ((++wait_seconds))
+}
+
+/opt/Unity/Editor/Unity -batchmode -nographics -projectPath /project -runTests -testResults /project/testResults.xml -logFile /project/Editor.log
+
+wait_file /project/testResults.xml 60
+
 echo "::set-output name=testResultFile::testResults.xml" 
 echo "::set-output name=editorLogFile::Editor.log" 
