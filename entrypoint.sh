9e8f3cf720ef2c35a0bd0e8691fb0805bf32efd0
integraldx <integraldx@integraldx.me>
2020-09-01 05:28:00 +0900

    몰라 이게 먹히나

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-time=$(date)
-echo "::set-output name=time::$time"
+/opt/Unity/Editor/Unity -batchmode -projectPath /project -runTests -testResults /project/testResults.xml -logFile /project/Editor.log -quit
+echo "::set-output name=testResultFile::testResults.xml" 
+echo "::set-output name=editorLogFile::Editor.log" 
