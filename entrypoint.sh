28da4017f7cd871a340a25dc9287c7d82d802590
integraldx <integraldx@integraldx.me>
2020-09-01 12:40:52 +0900

    Serial?

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,12 +12,14 @@ wait_file() {
 if [ -d "/workspace" ]
 then
     echo "Running unity..."
-    /opt/Unity/Editor/Unity -username ${UNITY_USERNAME} -password ${UNITY_PASSWORD} -batchmode -nographics -projectPath /workspace -runTests -testResults /workspace/testResults.xml -logFile /workspace/Editor.log
+    /opt/Unity/Editor/Unity -serial ${UNITY_SERIAL} -batchmode -nographics -projectPath /workspace -runTests -testResults /workspace/testResults.xml -logFile /workspace/Editor.log
     echo "Waiting for test result..."
     wait_file /workspace/testResults.xml 60
     cat /workspace/testResults.xml
     cat /workspace/Editor.log
     cat /workspace/ProjectSettings/ProjectSettings.asset
+    /opt/Unity/Editor/Unity -returnlicense -batchmode -quit
+    sleep 10
 else
     echo "Error: /workspace not found."
     exit 1
